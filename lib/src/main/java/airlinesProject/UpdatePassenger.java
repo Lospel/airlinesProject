02b62b28b36db10a7f1324e527e5499743f4ebc6
KimDowi <rlaehdnl@gmail.com>
2023-11-03 21:35:07 +0900

    2023-11-03

--- lib/src/main/java/airlinesProject/UpdatePassenger.java
+++ lib/src/main/java/airlinesProject/UpdatePassenger.java
@@ -40,7 +40,7 @@ public class UpdatePassenger extends JFrame implements ActionListener
 			System.out.printf("Exception [Err_Msg]: {%s}", ex.getMessage());
 		}
 		
-		l1 = new JLabel("승객 상세정보 수정하기");
+		l1 = new JLabel("승객 상세정보");
 		l2 = new JLabel("아이디");
 		l3 = new JLabel("이름");
 		l4 = new JLabel("나이");
@@ -52,6 +52,116 @@ public class UpdatePassenger extends JFrame implements ActionListener
 		l10 = new JLabel("성별");
 		l11 = new JLabel("여권");
 		
+		tf1 = new JTextField();
+		tf2 = new JTextField();
+		tf3 = new JTextField();
+		tf4 = new JTextField();
+		tf5 = new JTextField();
+		tf6 = new JTextField();
+		tf7 = new JTextField();
+		tf8 = new JTextField();
+		tf9 = new JTextField();
+		
+		bt1 = new JButton("수정하기");
+		bt2 = new JButton("나가기");
+		
+		l1.setHorizontalAlignment(JLabel.CENTER);
+		
+		bt1.addActionListener(this);
+		bt2.addActionListener(this);
+		
+		l1.setFont(f);
+		l2.setFont(f1);
+		l3.setFont(f1);
+		l4.setFont(f1);
+		l5.setFont(f1);
+		l6.setFont(f1);
+		l7.setFont(f1);
+		l8.setFont(f1);
+		l9.setFont(f1);
+		l10.setFont(f1);
+		l11.setFont(f1);
+		ch.setFont(f1);
+		
+		tf1.setFont(f1);
+		tf2.setFont(f1);
+		tf3.setFont(f1);
+		tf4.setFont(f1);
+		tf5.setFont(f1);
+		tf6.setFont(f1);
+		tf7.setFont(f1);
+		tf8.setFont(f1);
+		tf9.setFont(f1);
+		
+		bt1.setFont(f1);
+		bt2.setFont(f1);
+		
+		bt1.setBackground(Color.blue);
+		bt2.setBackground(Color.red);
+		
+		bt1.setForeground(Color.white);
+		bt2.setForeground(Color.white);
+		
+		p1 = new JPanel();
+		p1.setLayout(new GridLayout(1, 1, 10, 10));
+		p1.add(l1);
+		
+		p2 = new JPanel();
+		p2.setLayout(new GridLayout(11, 2, 10, 10));
+		p2.add(l2);
+		p2.add(ch);
+		p2.add(l3);
+		p2.add(tf1);
+		p2.add(l4);
+		p2.add(tf2);
+		p2.add(l5);
+		p2.add(tf3);
+		p2.add(l6);
+		p2.add(tf4);
+		p2.add(l7);
+		p2.add(tf5);
+		p2.add(l8);
+		p2.add(tf6);
+		p2.add(l9);
+		p2.add(tf7);
+		p2.add(l10);
+		p2.add(tf8);
+		p2.add(l11);
+		p2.add(tf9);
+		p2.add(bt1);
+		p2.add(bt2);
+		
+		p3 = new JPanel();
+		p3.setLayout(new GridLayout(1, 1, 10, 10));
+		
+		ImageIcon img = new ImageIcon(ClassLoader.getSystemResource("airlinesProject/icons/update.png"));
+		Image img1 = img.getImage().getScaledInstance(300, 500, Image.SCALE_SMOOTH);
+		ImageIcon ic1 = new ImageIcon(img1);
+		l12 = new JLabel(ic1);
+		
+		p3.add(l12);
+		
+		setLayout(new BorderLayout(10, 10));
+		add(p1, "North");
+		add(p2, "Center");
+		add(p3, "West");
+		
+		ch.addMouseListener(new MouseAdapter() 
+		{
+			@Override
+			public void mouseClicked(MouseEvent arg0) 
+			{
+				try 
+				{
+					ConnectionClass obj2 = new ConnectionClass();
+					String username = ch.getSelectedItem();
+				} 
+				catch (Exception e) 
+				{
+					// TODO: handle exception
+				}
+			}
+		});
 	}
 	
 	@Override
