a082f3c1fb4856106e418ece8e7774d8131f8d9f
KimDowi <rlaehdnl@gmail.com>
2023-11-07 18:21:57 +0900

    2023-11-07

--- lib/src/main/java/airlinesProject/FlightZone.java
+++ lib/src/main/java/airlinesProject/FlightZone.java
@@ -7,6 +7,10 @@ import java.sql.*;
 
 public class FlightZone extends JFrame
 {
+	private JTable table;
+	private JTextField t;
+	Choice ch1;
+	
 	FlightZone()
 	{
 		getContentPane().setBackground(new java.awt.Color(77,157,227));
@@ -29,7 +33,54 @@ public class FlightZone extends JFrame
 		flgihtDetails.setBounds(250,20,570,35);
 		add(flgihtDetails);
 		
+		JButton bt = new JButton("상세정보 보기");
+		bt.setFont(new Font("궁서체", Font.BOLD, 20));
+		bt.addActionListener(new ActionListener() 
+		{
+			
+			@Override
+			public void actionPerformed(ActionEvent e) 
+			{
+				try 
+				{
+					
+				} 
+				catch (Exception ex) 
+				{
+					System.out.printf("Exception [Err_Msg]: {%s}", ex.getMessage());
+				}
+			}
+			
+		});
+		bt.setBounds(560,100,220,30);
+		add(bt);
 		
+		table = new JTable();
+		table.setBackground(Color.white);
+		table.setBounds(23,250,800,300);
+		table.setFont(new Font("궁서체", Font.BOLD, 14));
+		add(table);
+		
+		ch1 = new Choice();
+		ch1.setBounds(290,103,200,35);
+		ch1.setFont(new Font("궁서체", Font.BOLD, 18));
+		try 
+		{
+			ConnectionClass obj = new ConnectionClass();
+			String q = "Select distinct f_code from flight";
+			ResultSet rest = obj.stm.executeQuery(q);
+			while(rest.next())
+			{
+				ch1.add(rest.getString("f_code"));
+				
+			}
+		} 
+		catch (Exception ex) 
+		{
+			System.out.printf("Exception [Err_Msg]: {%s}", ex.getMessage());
+		}
+		
+		add(ch1);
 	}
 	public static void main(String[] args) 
 	{
